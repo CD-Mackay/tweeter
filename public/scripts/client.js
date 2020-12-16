23660356b1a3bfbda312fa0d6349d0ed361a8fea
CD-Mackay <connor.mackay@gmail.com>
2020-12-16 21:58:07 +0000

    refactor to prevent cross-site scripting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
- 
   const loadTweets = function() {
     $.ajax({method: 'GET',
             url: '/tweets'})
@@ -15,20 +14,26 @@ $(document).ready(function() {
   const renderTweets = function(tweets) {
     let result = "";
     for (let tweet of tweets) {
-      result = result.concat(createTweetElement(tweet));
+      result = createTweetElement(tweet).concat(result);
     }
+    $('.tweets').empty();
     $('.tweets').append(result);
   };
 
   function getDate(milliseconds) {
     return new Date(milliseconds).toDateString();
   }
+  const escape = function(string) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(string));
+    return div.innerHTML
+  }
 
   const createTweetElement = function(tweet) {
     let user = tweet.user;
     let date = getDate(tweet.created_at);
-    let $tweet = ` <article class="tweet"><header><div class="username-icon"><p>${tweet.user.name}</p><p><img src=${tweet.user.avatars}/></p></div><p class="handle">${user.handle}</p></header>
-  <div><p class="tweet-content">${tweet.content.text}</p></div>
+    let $tweet = ` <article class="tweet"><header><div class="username-icon"><p>${escape(tweet.user.name)}</p><p><img src=${escape(tweet.user.avatars)}/></p></div><p class="handle">${escape(user.handle)}</p></header>
+  <div><p class="tweet-content">${escape(tweet.content.text)}</p></div>
   <footer><p>${date}</p>
     <div>
       <i class="far fa-flag"></i><i class="far fa-heart"></i><i class="fas fa-retweet"></i>
@@ -39,7 +44,6 @@ $(document).ready(function() {
   };
 
   loadTweets();
-
 //////////////////////////////
     $('.tweet-form').on('submit', function(event) {
       event.preventDefault();
@@ -53,12 +57,11 @@ $(document).ready(function() {
               url: '/tweets',
             data: $(this).serialize(),
           })
-      .then(function (data) {
-        console.log('Sucess: ', data);
-       $('.tweets').append(createTweetElement(data));
-        
-        
-      })
+      .then(function () {
+        loadTweets();
+        })
+       
     }})
     
+
 });
